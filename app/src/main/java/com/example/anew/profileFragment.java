17f98c1b7db142f3f3c7e1340c9e5752126e7fe6
danushan99 <danuthasan08@gmail.com>
2021-09-18 12:43:01 +0530

    user done

--- app/src/main/java/com/example/anew/profileFragment.java
+++ app/src/main/java/com/example/anew/profileFragment.java
@@ -1,16 +1,18 @@
 package com.example.anew;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 
-public class profileFragment extends Fragment {
+public class profileFragment extends Fragment implements View.OnClickListener {
 
-
+    private Button editbtn,deactivBtn,changpaswd;
     public profileFragment() {
         // Required empty public constructor
     }
@@ -20,6 +22,30 @@ public class profileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View myView=inflater.inflate(R.layout.fragment_profile,container,false);
+        // for edit profile user fragment
+        editbtn=(Button) myView.findViewById(R.id.btnedit1);
+        editbtn.setOnClickListener(this);
+        //for delete profile user fragment
+        deactivBtn=(Button) myView.findViewById(R.id.deactivBtn);
+        deactivBtn.setOnClickListener(this);
+        //for deActive account from user fragment
+        changpaswd=(Button) myView.findViewById(R.id.changpaswd);
+        changpaswd.setOnClickListener(this);
+        return myView;
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnedit1:
+                startActivity(new Intent(this.requireContext(),EditProfile.class));
+                break;
+            case R.id.deactivBtn:
+                startActivity(new Intent(this.requireContext(),DeleteAccount.class));
+                break;
+            case R.id.changpaswd:
+                startActivity(new Intent(this.requireContext(),passwordReset.class));
+                break;
+        }
     }
 }

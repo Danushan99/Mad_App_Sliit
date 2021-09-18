6d74cf1c62f0e43c2c025cd6c3ec18c629af6133
danushan99 <danuthasan08@gmail.com>
2021-09-18 18:59:34 +0530

    bill

---
+++ app/src/main/java/com/example/anew/Doc_bill.java
@@ -0,0 +1,14 @@
+package com.example.anew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Doc_bill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_bill);
+    }
+}

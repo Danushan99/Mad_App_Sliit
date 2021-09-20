c81bf0127309d025187c2f8c1f078bd54d5b61df
danushan99 <danuthasan08@gmail.com>
2021-09-20 19:58:54 +0530

    befor db connect

--- app/src/main/java/com/example/anew/HomeFragment.java
+++ app/src/main/java/com/example/anew/HomeFragment.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 
-public class HomeFragment extends Fragment  {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     SliderView sliderView;
     int[] images = {R.drawable.slider2,
@@ -27,7 +29,10 @@ public class HomeFragment extends Fragment  {
                              Bundle savedInstanceState) {
         //setContentView(R.layout.fragment_home);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+//        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        Button homebtn1 = (Button) view.findViewById(R.id.homebtn1);
+        homebtn1.setOnClickListener(this);
 
         sliderView = view.findViewById(R.id.image_slider);
         SliderAdapter sliderAdapter = new SliderAdapter(images);
@@ -39,5 +44,28 @@ public class HomeFragment extends Fragment  {
 
         return view;
     }
+    @Override
+    public void onClick(View view) {
+        Fragment fragment = null;
+        switch (view.getId()) {
+            case R.id.homebtn1:
+                fragment = new DoctorFragment();
+                replaceFragment(fragment);
+                break;
+
+//            case R.id.phbookButton:
+//                fragment = new PhoneBookFragment();
+//                replaceFragment(fragment);
+//                break;
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.content_frame, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 
 }

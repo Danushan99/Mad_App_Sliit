e6deafbfc549d5d9c2b576da5735e8cc2cde2164
danushan99 <danuthasan08@gmail.com>
2021-09-24 21:59:45 +0530

    dhanu ui feg

--- app/src/main/java/com/example/anew/HomeFragment.java
+++ app/src/main/java/com/example/anew/HomeFragment.java
@@ -15,15 +15,15 @@ import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
+public class HomeFragment extends Fragment  {
 
-//    SliderView sliderView;
-//    int[] images = {R.drawable.slider2,
-//            R.drawable.slider1,
-//            R.drawable.slider3,
-//            R.drawable.slider6,
-//            R.drawable.slider5,
-//            R.drawable.slider4};
+    SliderView sliderView;
+    int[] images = {R.drawable.slider2,
+            R.drawable.slider1,
+            R.drawable.slider3,
+            R.drawable.slider6,
+            R.drawable.slider5,
+            R.drawable.slider4};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,42 +31,41 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         //setContentView(R.layout.fragment_home);
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 //        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+//
+//        Button homebtn1 = (Button) view.findViewById(R.id.homebtn1);
+//        homebtn1.setOnClickListener(this);
 
-        Button homebtn1 = (Button) view.findViewById(R.id.homebtn1);
-        homebtn1.setOnClickListener(this);
-
-//        sliderView = view.findViewById(R.id.image_slider);
-//        SliderAdapter sliderAdapter = new SliderAdapter(images);
-//        sliderView.setSliderAdapter(sliderAdapter);
-//        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
-//        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
-//        sliderView.startAutoCycle();
+        sliderView = view.findViewById(R.id.image_slider);
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
         // Inflate the layout for this fragment
 
         return view;
     }
-    @Override
-    public void onClick(View view) {
-        Fragment fragment = null;
-        switch (view.getId()) {
-            case R.id.homebtn1:
-                fragment = new DoctorFragment();
-                replaceFragment(fragment);
-                break;
+//    @Override
+//    public void onClick(View view) {
+//        Fragment fragment = null;
+//        switch (view.getId()) {
+//            case R.id.homebtn1:
+//                fragment = new DoctorFragment();
+//                replaceFragment(fragment);
+//                break;
 
 //            case R.id.phbookButton:
 //                fragment = new PhoneBookFragment();
 //                replaceFragment(fragment);
 //                break;
         }
-    }
+    //}
 
-    public void replaceFragment(Fragment someFragment) {
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.content_frame, someFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
+//    public void replaceFragment(Fragment someFragment) {
+//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//        transaction.replace(R.id.content_frame, someFragment);
+//        transaction.addToBackStack(null);
+//        transaction.commit();
 
 
-}
+

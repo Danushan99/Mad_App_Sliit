ff175f4502c2388455576cf0e4ad857c4863d0ae
danushan99 <danuthasan08@gmail.com>
2021-09-25 23:58:58 +0530

    Doc freg

--- app/src/main/java/com/example/anew/doctor/DoctorFragment.java
+++ app/src/main/java/com/example/anew/doctor/DoctorFragment.java
@@ -14,37 +14,16 @@ import com.example.anew.R;
 
 public class DoctorFragment extends Fragment implements View.OnClickListener {
 
-    private CardView card1,car2;
-
-
-
-    public DoctorFragment() {
-
-    }
-
-
-
+    private CardView card1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-
         View myView=inflater.inflate(R.layout.fragment_doctor,container,false);
         card1=(CardView) myView.findViewById(R.id.dcard1);
         card1.setOnClickListener(this);
-
         return myView;
-
-
-
-
-
-
-
     }
-
-
     @Override
     public void onClick(View v) {
         switch (v.getId()){

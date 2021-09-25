3334c9f25f40f39b643d50282e1a7b123ef57105
IT20116392 <it20116392@my.sliit.lk>
2021-09-25 23:18:46 +0530

    Jivishan_changed

--- app/src/main/java/com/example/anew/user/DeleteAccount.java
+++ app/src/main/java/com/example/anew/user/DeleteAccount.java
@@ -28,8 +28,10 @@ public class DeleteAccount extends AppCompatActivity {
         mFirebaseInstance = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseInstance.getReference("Users");
 
+
         delete=findViewById(R.id.button6);
         delete.setOnClickListener(new View.OnClickListener()
+
         {
             @Override
             public void onClick(View v) {
@@ -46,6 +48,7 @@ public class DeleteAccount extends AppCompatActivity {
 
             }
         });
+
 //jivishan_delete_account
         DleCancel = findViewById(R.id.DleCancel);
         DleCancel.setOnClickListener(new View.OnClickListener() {
